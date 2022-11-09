b3e50cfaa6e641e717b33f1cd61213806354de5e
José Ignacio Escribano <jiep@users.noreply.github.com>
2022-11-09 16:24:24 +0000

    Add emojified hashes for SHA256

--- tests/sha2_utils.rs
+++ tests/sha2_utils.rs
@@ -4,7 +4,7 @@ use sha2_utils::{sha256, sha512};
 fn sha256_works() {
     let result = sha256(&b"hello world".to_vec());
     assert_eq!(
-        result,
+        hex::encode(result),
         "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
     );
 }
@@ -12,5 +12,5 @@ fn sha256_works() {
 #[test]
 fn sha512_works() {
     let result = sha512(&b"hello world".to_vec());
-    assert_eq!(result, "309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f");
+    assert_eq!(hex::encode(result), "309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f");
 }
